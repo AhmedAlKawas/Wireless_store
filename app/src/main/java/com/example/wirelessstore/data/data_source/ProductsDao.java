dba88519f8a3d40b86b25305a0e7e9a18146d39a
AhmedAlKawas <ahmed.alkawas77@gmail.com>
2022-08-02 00:19:30 +0200

    Adding Products Dao

---
+++ app/src/main/java/com/example/wirelessstore/data/data_source/ProductsDao.java
@@ -0,0 +1,22 @@
+package com.example.wirelessstore.data.data_source;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.example.wirelessstore.domain.model.Product;
+
+import java.util.List;
+
+@Dao
+public interface ProductsDao {
+
+    @Query("SELECT * from products_table")
+    MutableLiveData<List<Product>> getProducts();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertProduct(Product product);
+
+}
