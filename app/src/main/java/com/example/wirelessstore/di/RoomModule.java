7e802f5d4660308a736889946d6c0a37c92ae40f
AhmedAlKawas <ahmed.alkawas77@gmail.com>
2022-08-02 11:15:47 +0200

    Implementing app and room modules

---
+++ app/src/main/java/com/example/wirelessstore/di/RoomModule.java
@@ -0,0 +1,41 @@
+package com.example.wirelessstore.di;
+
+import android.app.Application;
+
+import androidx.room.Room;
+
+import com.example.wirelessstore.data.data_source.AppDatabase;
+import com.example.wirelessstore.data.repository.ProductsRepositoryImpl;
+import com.example.wirelessstore.domain.repository.ProductsRepository;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class RoomModule {
+
+    private final AppDatabase database;
+
+    public RoomModule(Application mApplication) {
+        database = Room.databaseBuilder(
+                mApplication,
+                AppDatabase.class,
+                "app_database"
+        ).build();
+    }
+
+    @Singleton
+    @Provides
+    AppDatabase providesRoomDatabase() {
+        return database;
+    }
+
+    @Singleton
+    @Provides
+    ProductsRepository productsRepository(AppDatabase database) {
+        return new ProductsRepositoryImpl(database.productsDao());
+    }
+
+}
